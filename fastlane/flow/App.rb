ebb6263065c0a3aede2b68ec672aa864cddc1440
Hans Seiffert <hans.seiffert@smartmobilefactory.com>
2018-02-28 14:01:27 +0100

    Fix not found build variant

--- fastlane/flow/App.rb
+++ fastlane/flow/App.rb
@@ -253,7 +253,7 @@ private_lane :smf_test_app_project do |options|
   project_config = options[:project_config]
   build_variant = options[:build_variant]
   build_variants_config = options[:build_variants_config]
-  build_variant_config = build_variants_config[build_variant]
+  build_variant_config = build_variants_config["targets"][build_variant]
   should_perform_unit_test = (build_variant_config["perform_unit_tests"].nil? ? false : build_variant_config["perform_unit_tests"])
   should_clean_project = (options[:should_clean_project].nil? ? true : options[:should_clean_project])
   use_sigh = (options[:use_sigh].nil? ? true : options[:use_sigh])
