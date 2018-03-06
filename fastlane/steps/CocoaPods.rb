8b9389084cc7ace661250a1ae724ec45e3be629d
Hans Seiffert <hans.seiffert@smartmobilefactory.com>
2018-03-06 18:58:11 +0100

    Try to fix Pod install call

--- fastlane/steps/CocoaPods.rb
+++ fastlane/steps/CocoaPods.rb
@@ -6,7 +6,7 @@ desc "Runs pod install if the project contains a Podfile"
 private_lane :smf_install_pods_if_project_contains_podfile do |options|
 
   if File.exist?("#{smf_workspace_dir}/Podfile")
-    sh "pod install"
+    sh "cd #{smf_workspace_dir}; pod install"
   else
     UI.message("Didn't install Pods as the project doesn't contain a Podfile")
   end
