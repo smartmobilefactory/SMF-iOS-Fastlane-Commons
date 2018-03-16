4387804fc81295f6bc36d426a4f8c2f6f02021ca
Hans Seiffert <hans.seiffert@smartmobilefactory.com>
2018-03-16 13:44:11 +0100

    Declare html report output

--- fastlane/steps/Xcode-Build.rb
+++ fastlane/steps/Xcode-Build.rb
@@ -134,7 +134,7 @@ private_lane :smf_perform_unit_tests do |options|
     destination: destination,
     code_coverage: true,
     output_types: "html,junit,json-compilation-database",
-    output_files: "report.xml,report.junit,report.json"
+    output_files: "report.html,report.xml,report.junit,report.json"
     )
 
   ENV[$SMF_DID_RUN_UNIT_TESTS_ENV_KEY] = "true"
