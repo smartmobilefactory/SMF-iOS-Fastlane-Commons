384530a6e530c149a32d795c8381dea50bb202cb
Bartosz Świątek <bartosz@smartmobilefactory.com>
2018-10-15 16:52:42 +0200

    Be less explicit

--- fastlane/steps/Xcode-Build.rb
+++ fastlane/steps/Xcode-Build.rb
@@ -43,7 +43,7 @@ private_lane :smf_archive_ipa do |options|
   upload_itc = (build_variant_config[:upload_itc].nil? ? false : build_variant_config[:upload_itc])
   upload_bitcode = (build_variant_config[:upload_bitcode].nil? ? true : build_variant_config[:upload_bitcode])
 
-  use_xcconfig = build_variant_config[:use_xcconfig].nil? ? false : build_variant_config[:use_xcconfig]
+  use_xcconfig = build_variant_config[:xcconfig_name].nil? ? false : true
   xcconfig_name = use_xcconfig ? build_variant_config[:xcconfig_name] : "Release"
   output_name = use_xcconfig ? "#{scheme}-#{xcconfig_name}" : scheme
   
@@ -125,7 +125,7 @@ private_lane :smf_perform_unit_tests do |options|
   project_name = @smf_fastlane_config[:project][:project_name]
   build_variant_config = @smf_fastlane_config[:build_variants][@smf_build_variant_sym]
   device = build_variant_config["tests.device_to_test_against".to_sym]
-  use_xcconfig = build_variant_config[:use_xcconfig].nil? ? false : build_variant_config[:use_xcconfig]
+  use_xcconfig = build_variant_config[:xcconfig_name].nil? ? false : true
   xcconfig_name = use_xcconfig ? build_variant_config[:xcconfig_name] : "Release"
 
   # Prefer the unit test scheme over the normal scheme
