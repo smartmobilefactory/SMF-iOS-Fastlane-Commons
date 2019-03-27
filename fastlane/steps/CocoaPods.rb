813868319f7a4ad21f90ec021cae0f9bb9789136
UrsKahmann <urs@smartmobilefactory.com>
2019-03-27 09:52:26 +0100

    Removed manual pod install with shell command and replaced it with the cocoapods fastlane action.

--- fastlane/steps/CocoaPods.rb
+++ fastlane/steps/CocoaPods.rb
@@ -5,10 +5,12 @@
 desc "Runs pod install if the project contains a Podfile"
 private_lane :smf_install_pods_if_project_contains_podfile do |options|
 
-  if File.exist?("#{smf_workspace_dir}/Podfile")
-  	sh "which pod"
-  	sh "pod --version"
-    sh "cd #{smf_workspace_dir}; pod install"
+  podfile = "#{smf_workspace_dir}/Podfile"
+
+  if File.exist?(podfile)
+    cocoapods(
+      podfile: podfile
+    )
   else
     UI.message("Didn't install Pods as the project doesn't contain a Podfile")
   end
