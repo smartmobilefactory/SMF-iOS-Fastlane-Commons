5ab51067eab5a33365dd3bf0ea487e9d1e7f5111
Alexandru Taras <alex.taras@gmx.com>
2019-04-01 17:12:41 +0300

    added some logging

--- fastlane/steps/Pipeline.rb
+++ fastlane/steps/Pipeline.rb
@@ -4,10 +4,17 @@
 
 desc "Generates a jenkins file based on the fastlane commons template and options from Config.json"
 private_lane :smf_generate_jenkins_file do |options|
+	UI.important("Reading template")
+	UI.important("Workspace #{smf_workspace_dir}")
+
 	jenkinsFileData = File.read("#{@fastlane_commons_dir_path}/pipeline/App_Jenkinsfile")
  	build_variants = ["Alpha", "Beta", "Live"]
  	
+ 	UI.important("Default variants #{build_variants}")
+
  	build_variants_from_config = @smf_fastlane_config[:build_variants].keys
+
+	UI.important("Config variants #{build_variants_from_config}")
  	
  	build_config_variants = []
  	build_variants_from_config.each do |variant|
