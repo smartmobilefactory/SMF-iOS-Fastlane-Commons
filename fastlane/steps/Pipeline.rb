3f017c5bb812ee649b3d395943a7303960cbcd0c
Alexandru Taras <alex.taras@gmx.com>
2019-04-01 17:50:10 +0300

    ruby syntax fix

--- fastlane/steps/Pipeline.rb
+++ fastlane/steps/Pipeline.rb
@@ -33,7 +33,7 @@ private_lane :smf_update_jenkins_file do |options|
 	something_to_commit = false
 
 	Dir.chdir(smf_workspace_dir) do
-    	something_to_commit = !`git status --porcelain`.include? "Jenkinsfile"
+    	something_to_commit = !`git status --porcelain`.include? 'Jenkinsfile'
   	end
 
   	UI.message("Checking for Jenkins file changes...")
@@ -49,11 +49,11 @@ private_lane :smf_update_jenkins_file do |options|
     	git_add(path: '.')
     	git_commit(path: '.', message: "Updated Jenkins file")
 
-    	push_to_git_remote(
-      		remote: 'origin',
-      		remote_branch: ENV["CHANGE_BRANCH"],
-      		force: false
-    	)
+    	# push_to_git_remote(
+     #  		remote: 'origin',
+     #  		remote_branch: ENV["CHANGE_BRANCH"],
+     #  		force: false
+    	# )
 
   		UI.user_error!("Build will be restarted. This is not a failure.")
   	else
