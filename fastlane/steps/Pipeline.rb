a2a8a2ba3b908190774e5a27ad5f02f4211c4776
Alexandru Taras <alex.taras@gmx.com>
2019-04-03 10:29:45 +0300

    simpler push

--- fastlane/steps/Pipeline.rb
+++ fastlane/steps/Pipeline.rb
@@ -82,15 +82,11 @@ private_lane :smf_update_jenkins_file do |options|
 	# If something changed in config
 	if something_to_commit
 		UI.message("Jenkinsfile changed since last build, will synchronize and commit the changes...")
-		
+
 		git_add(path: "./#{JENKINSFILE_FILENAME}")
 		git_commit(path: ".", message: "Updated Jenkinsfile")
 
-		push_to_git_remote(
-		remote: "origin",
-			remote_branch: ENV["CHANGE_BRANCH"],
-			force: false
-		)
+		push_to_git_remote
 
 		UI.user_error!("Jenkinsfile changed since last build, build will be restarted. This is not a failure.")
 	else
