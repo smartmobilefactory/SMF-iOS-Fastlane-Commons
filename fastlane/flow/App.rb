f08c83a8d31bd456fa58dbeb229947f214e67b00
Alexandru Taras <alex.taras@gmx.com>
2019-04-12 17:14:57 +0300

    Added some logging for test

--- fastlane/flow/App.rb
+++ fastlane/flow/App.rb
@@ -75,6 +75,7 @@ private_lane :smf_deploy_build_variant do |options|
   use_hockey = (build_variant_config[:use_hockey].nil? ? true : build_variant_config[:use_hockey])
   
   use_sentry = (build_variant_config[:sentry_org_slug] != nil && build_variant_config[:sentry_project_slug] != nil)
+  UI.message("Will upload to Sentry: #{use_sentry}")
 
   # The default value of push_generated_code depends on whether Strings are synced with PhraseApp. If PhraseApp should be synced, the default is true
   push_generated_code = (build_variant_config[:push_generated_code].nil? ? (build_variant_config[:phrase_app_script] != nil) : build_variant_config[:push_generated_code])
@@ -196,10 +197,12 @@ private_lane :smf_deploy_build_variant do |options|
   if use_sentry
     UI.message("Dsym file will be uploaded to Sentry...")
 
+    project_config = @smf_fastlane_config[:project]
+
     sentry_upload_dsym(
       auth_token: ENV["SENTRY_AUTH_TOKEN"],
-      org_slug: build_variant_config[:sentry_org_slug],
-      project_slug: build_variant_config[:sentry_project_slug],
+      org_slug: project_config[:sentry_org_slug],
+      project_slug: project_config[:sentry_project_slug],
       url: ENV["SENTRY_URL"]
     )
   end
