97b17417b11b2ba9467f74c13656de0a44efdffd
kondeichmann <k.deichmann@mac.com>
2019-04-26 09:17:35 +0200

    check if custom_credentials exists before generating

--- fastlane/steps/Pipeline.rb
+++ fastlane/steps/Pipeline.rb
@@ -110,13 +110,15 @@ private_lane :smf_generate_setup_files do |options|
 
 	jenkinsFileData = jenkinsFileData.gsub("#{BUILD_VARIANTS_PATTERN}", JSON.dump(build_variants_from_config))
 
-  for custom_credential in CUSTOM_CREDENTIALS
-    if @smf_fastlane_config[:project][:custom_credentials][custom_credential.to_sym]
-			custom_credential_key = @smf_fastlane_config[:project][:custom_credentials][custom_credential.to_sym]
-      custom_credential_string = "#{custom_credential_key} = credentials('#{custom_credential_key}')"
-			jenkinsFileData = jenkinsFileData.gsub(custom_credential, custom_credential_string)
-    else
-			jenkinsFileData = jenkinsFileData.gsub(custom_credential, "")
+  if @smf_fastlane_config[:project][:custom_credentials]
+    for custom_credential in CUSTOM_CREDENTIALS
+      if @smf_fastlane_config[:project][:custom_credentials][custom_credential.to_sym]
+			  custom_credential_key = @smf_fastlane_config[:project][:custom_credentials][custom_credential.to_sym]
+        custom_credential_string = "#{custom_credential_key} = credentials('#{custom_credential_key}')"
+			  jenkinsFileData = jenkinsFileData.gsub(custom_credential, custom_credential_string)
+      else
+			  jenkinsFileData = jenkinsFileData.gsub(custom_credential, "")
+      end
     end
   end
 
